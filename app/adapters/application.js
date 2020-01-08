508768f6ee97b87a84f24dd25ddb38855d01b92a
trdp30 <trdp30@gmail.com>
2020-01-08 21:56:25 +0530

    endpoint updated from http to https

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: "http://homswag.herokuapp.com",
+  host: "https://homswag.herokuapp.com",
   namespace: "api/v1",
 });
