25895f0898e778efb2fc68d84d3721c805240d95
trdp30 <trdp30@gmail.com>
2020-01-28 20:55:19 +0530

    emberfire installed

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,3 +4,10 @@ export default DS.RESTAdapter.extend({
   host: "https://homswag.herokuapp.com",
   namespace: "api/v1",
 });
+// import FirestoreAdapter from 'emberfire/adapters/firestore';
+
+// export default FirestoreAdapter.extend({
+//     // Uncomment the following lines to enable offline persistence and multi-tab support
+//     // enablePersistence: true,
+//     // persistenceSettings: { synchronizeTabs: true }
+// });
