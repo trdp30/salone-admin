1a2cde5ebb6d6b7a35c34a70e53c8d4ca73eeaee
trdp30 <trdp30@gmail.com>
2020-03-23 20:12:50 +0530

    token updated

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,7 +6,7 @@ export default DS.RESTAdapter.extend({
   namespace: "api/v1",
   headers: computed(function() {
     return {
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTksImlhdCI6MTU4NDA4NDY3NiwiZXhwIjoxNTg0OTQ4Njc2fQ.FvbEpwHIRMvwJ91xjkSVqyvJ4SF5JKvcTehTakZObAM",
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNTg0OTc0NTAzLCJleHAiOjE1ODU4Mzg1MDN9.0bhYoWsd8n-oV0wusG4DCTNuW7X-ewgXI6qv8L_Ox6I",
     }
   }),
   handleResponse: function (status,headers,payload,requestData){
