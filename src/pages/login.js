3f94d0c33d3c2dc5e677ba40e55b817f12940ffb
manoj <manoj.upadhyay@talview.com>
2020-06-12 14:38:01 +0530

    Login

--- src/pages/login.js
+++ src/pages/login.js
@@ -37,6 +37,7 @@ function Login(props) {
     submitUserName(email)
   }
   return (
+    <>
     <form onSubmit={onSubmit}>
       <div className="form-group">
         <label htmlFor="inputEmail1">Email address</label>
@@ -64,6 +65,49 @@ function Login(props) {
       <button type="button" className="btn btn-primary" onClick={() => generateOtp()}>Generate Otp</button>
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
+
+{/* <>
+ <div class="container" style={{paddingTop : "14vh"}}> 
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3 style={{textAlign : 'center'}}>Member Login</h3>
+			</div>
+			<div class="card-body">
+				<form>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" placeholder="username"/>
+					</div>
+
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" placeholder="password"/>
+					</div>
+
+					<div class="row align-items-center remember">
+						<input type="checkbox"/>Remember Me
+					</div>
+
+					<div class="form-group">
+						<input type="submit" value="Login" class="btn float-right login_btn"/>
+					</div>
+          
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center">
+					<a href="#" style={{color : 'yellow'}}>Forgot your password?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div> */}
+</>
   )
 }
 
