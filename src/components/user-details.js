e30ca9cc2c292b7e704ce1aa6f39f00e93ffc1bc
manoj <manoj.upadhyay@talview.com>
2020-06-16 15:20:57 +0530

    User deails added

---
+++ src/components/user-details.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { findUser } from '../store/actions/user.action';
+
+function UserDetails(props) {
+  const { userModel, getUserById, user_id } = props
+
+  useEffect(() => {
+    if(!userModel.request.isLoading && !userModel.request.error && userModel.data.allIds && !userModel.data.allIds.length) {
+      getUserById(user_id)
+    }
+  }, [])
+
+  return (
+    <>
+      {userModel.data && userModel.data.byId && userModel.data.byId[user_id] && userModel.data.byId[user_id].name}
+    </>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  userModel: state.user,
+})
+
+const mapDispatchToProps = dispatch => ({
+  getUserById: (id) => dispatch(findUser(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserDetails)
--- src/pages/order/orders.js
+++ src/pages/order/orders.js
@@ -2,8 +2,11 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchOrders } from '../../store/actions/order.action';
+import Loading from '../../components/loading';
+import UserDetails from '../../components/user-details';
 
 function Orders(props) {
+
   const { orderModel } = props
   useEffect(() => {
     if(!orderModel.request.isLoading && !orderModel.request.error) {
@@ -12,16 +15,53 @@ function Orders(props) {
   }, [])
 
   return (
-    <ol>
-      {!props.orderModel.request.isLoading && props.orderModel.data.allIds && props.orderModel.data.allIds.length &&
-        props.orderModel.data.allIds.map((orderId) => (
-          <li key={orderId}>
-            <Link to={`/order/${orderId}/details`}>
-              {props.orderModel.data.byId[orderId] && props.orderModel.data.byId[orderId].address_id}
-            </Link>
-          </li>
-        ))}
-    </ol>
+    // <ol>
+    //   {!props.orderModel.request.isLoading && props.orderModel.data.allIds && props.orderModel.data.allIds.length &&
+    //     props.orderModel.data.allIds.map((orderId) => (
+    //       <li key={orderId}>
+    //         <Link to={`/order/${orderId}/details`}>
+    //           {props.orderModel.data.byId[orderId] && props.orderModel.data.byId[orderId].address_id}
+    //         </Link>
+    //       </li>
+    //     ))}
+    // </ol>
+    <>
+      {!props.orderModel.request.isLoading ? 
+        (
+          <div className="desktop-list" style={{marginTop : '24px', fontSize : '12px'}}>
+            <div className="table-responsive">
+              <table className="table">
+                <thead className="thead-dark">
+                  <tr>
+                    <th scope="col">A.No</th>
+                    <th scope="col">Customer Name</th>
+                    <th scope="col">Items</th>
+                    <th scope="col">Placed On</th>
+                    <th scope="col">Placed For</th>
+                    <th scope="col">Confirmed</th>
+                    <th scope="col">Total Paid</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Device</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {!props.orderModel.request.isLoading && props.orderModel.data.allIds && props.orderModel.data.allIds.length &&
+                  props.orderModel.data.allIds.map((orderId) => (
+                    <tr>
+                      <td>{props.orderModel.data.byId[orderId].id}</td>
+                      <td>
+                        <UserDetails user_id={props.orderModel.data.byId[orderId].id}/>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        ) :
+        <Loading/>
+      }
+    </>
   )
 }
 
